98f74f0e9bd8e851c8bd348307e21b12ec60433c
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-11-06 17:42:43 -0500

    create new puzzle still not made. Show feature with checkwin set up with side by side view

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -15,6 +15,14 @@ class PuzzlesController < ApplicationController
   end
 
   def create
+    @puzzle= Puzzle.new puzzle_params
+    @puzzle.user_id = current_user.id
+    @puzzle.save
+    redirect_to puzzle_path(@puzzle)
+  end
 
+  private
+  def puzzle_params
+    params.require(:puzzle).permit(:notes)
   end
 end
