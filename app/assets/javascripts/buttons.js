3f3ff8606af8fc53f706a96ecc5eb87baff5a461
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-12-13 21:43:08 -0500

    css for new and edit

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -2,7 +2,7 @@ var test="this is a test"
 
 //clear board- clears whatever board is currently on the page, long as it isn't static
 $(document).ready(function() {
-  $('#reset').on('click', function() {
+  $('.reset').on('click', function() {
 
     cubes=$('.board').not($('#static_board')).children().children().children();
       cubes.removeClass();
